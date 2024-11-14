284670d6418e8d0d842745dd5a7fbe95c6ea8bf8
joyanedel <joyanede@alumnos.inf.utfsm.cl>
2024-11-13 22:47:14 -0300

    Add tcp listener base code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+    let server = TcpListener::bind(("localhost", 8798)).expect("Couldn't bind port");
+
+    for stream in server.incoming() {
+        let client = match stream {
+            Ok(client) => {
+                println!(
+                    "Connection established: {}",
+                    client.local_addr().unwrap().ip()
+                );
+                client
+            }
+            Err(client_error) => {
+                eprintln!("Error accepting incoming request: {client_error}");
+                continue;
+            }
+        };
+
+        handle_connection(client);
+    }
+}
+
+fn handle_connection(client: TcpStream) {
+    println!("Boo");
 }
