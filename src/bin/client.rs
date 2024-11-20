1dbdf80e1c76ee8b66a2738c60e986daff533c87
joyanedel <joyanede@alumnos.inf.utfsm.cl>
2024-11-20 00:38:17 -0300

    Replace tokenizer by sqlparser

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -4,7 +4,12 @@ fn main() {
     let mut connection =
         TcpStream::connect(("localhost", 8798)).expect("Couldn't establish connection with server");
 
-    let _ = connection.write("CREATE CREATE TABLE hola".as_bytes());
+    let result = connection.write("CREATE TABLE my_table ( id VARCHAR(255) )".as_bytes());
+    if result.is_err() {
+        eprintln!("Error at writing: {}", result.unwrap_err());
+    } else {
+        println!("Bytes written: {}", result.unwrap());
+    }
 
     connection
         .shutdown(std::net::Shutdown::Both)
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use bytes::BytesMut;
+use sqlparser::{ast::Statement, dialect::GenericDialect, parser::Parser};
 use std::io;
-use tokenizer::TokenKind;
 use tokio::net::{TcpListener, TcpStream};
-
-pub mod tokenizer;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
@@ -17,16 +15,45 @@ async fn main() -> io::Result<()> {
 }
 
 fn handle_connection(client: TcpStream) {
-    let mut buffer = BytesMut::with_capacity(1024);
-    client.try_read_buf(&mut buffer).unwrap();
-    let raw_frame = String::from_utf8_lossy(buffer.as_ref());
+    let raw_frame = read_buffer(&client);
     println!("My buffer: {}", raw_frame);
 
     parse_frame(&raw_frame);
 }
 
+fn read_buffer(client: &TcpStream) -> String {
+    let mut buffer = BytesMut::with_capacity(1024);
+    loop {
+        match client.try_read_buf(&mut buffer) {
+            Ok(0) => break,
+            Ok(_) => continue,
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => continue,
+            Err(_) => panic!("this should not happen"),
+        }
+    }
+
+    String::from_utf8_lossy(buffer.as_ref()).to_string()
+}
+
 fn parse_frame(raw_frame: &str) {
-    let tokens: Vec<_> = raw_frame.split(" ").collect();
-    let token_kinds: Vec<_> = tokens.into_iter().map(TokenKind::try_from).collect();
-    println!("Tokens: {:?}", token_kinds);
+    let dialect = GenericDialect {};
+    let ast = Parser::parse_sql(&dialect, raw_frame);
+
+    if ast.is_err() {
+        return;
+    }
+
+    let ast = ast.unwrap();
+    let query = ast.first().unwrap();
+
+    if let Statement::CreateTable(ct_query) = query {
+        println!(
+            "\
+            Create Table
+            query Table name: {:?},
+            Columns: {:?}\
+        ",
+            ct_query.name, ct_query.columns
+        );
+    }
 }
